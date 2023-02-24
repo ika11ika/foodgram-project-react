5ff6ed43276946c156c328d7bd1232a99634d7da
Ирина Московкина <ika11ika@yandex.ru>
2023-02-24 23:50:06 +0300

    Added user model frame

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+USER = 'user'
+ADMIN = 'admin'
+
+ROLE_CHOICES = (
+    (USER, 'Аутентифицированный'),
+    (ADMIN, 'Администратор'),
+)
+
+
+class User(AbstractUser):
+    login = models.CharField(
+        max_length=150,
+        unique=True,
+        null=True,
+        verbose_name='Логин'
+    )
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль'
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='Email'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+    role = models.CharField(
+        'Роль',
+        blank=True,
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=USER,
+    )
+
+    class Meta:
+        ordering = ['login']
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
