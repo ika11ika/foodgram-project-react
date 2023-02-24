07efc8f97f835a3543f9bbc7552b93a5cd34b1af
Ирина Московкина <ika11ika@yandex.ru>
2023-02-24 23:56:37 +0300

    Added ingridient and tag models

--- backend/foodgram/recepies/models.py
+++ backend/foodgram/recepies/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    measurement_unit = models.CharField(
+        max_length=200,
+        verbose_name='Единицы измерения'
+    )
+    
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name} - {self.measurement_unit}'
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Адрес'
+    )
+    color = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='Цвет'
+    )
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
