7e6315686b06f1197484f79fc3056456ac85c19b
Ирина Московкина <ika11ika@yandex.ru>
2023-02-25 21:27:47 +0300

    Fixed app's name and empty files, added admin's panel

---
+++ backend/foodgram/recipes/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+
+
+from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingCart, Tag)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    list_filter = ('name',)
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'times_added')
+    list_filter = ('author', 'name', 'tags')
+
+    def times_added(self, obj):
+        return Favorite.objects.filter(recipe=obj).count()
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient')
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+
+
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(ShoppingCart)
+admin.site.register(Tag)
