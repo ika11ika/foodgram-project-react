4e74c1c683e7410efef59b54a4fbfb25791d13a8
ika11ika <ika11ika@yandex.ru>
2023-03-10 13:13:09 +0300

    Update settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 from dotenv import load_dotenv
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 load_dotenv()
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.getenv('SECRET_KEY', default='secret_key_default_value')
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key_default_value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
